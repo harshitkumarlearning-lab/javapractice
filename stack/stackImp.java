774ac19e8fb9cb352c1007788dcc6ee59dd87d3a
harshitkumarlearning-lab <harshitkumarlearning@gmail.com>
2026-08-28 12:37:26 +0530

    Stack added

---
+++ stack/stackImp.java
@@ -0,0 +1,14 @@
+package stack;
+import java.util.Stack;
+public class stackImp {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        System.out.println(st);
+        System.out.println(st.pop());
+        System.out.println(st.peek());
+        System.out.println(st);
+    }
+}
