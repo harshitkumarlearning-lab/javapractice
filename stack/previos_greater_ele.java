e6eda35354b1997cfdeb14fcd888c06bb2aa3eef
harshitkumarlearning-lab <harshitkumarlearning@gmail.com>
2026-09-04 11:48:56 +0530

    Previous gretest ele

---
+++ stack/previos_greater_ele.java
@@ -0,0 +1,32 @@
+package stack;
+import java.util.Scanner;
+import java.util.Stack;
+public class previos_greater_ele {
+    public static void pge(int [] arr){
+        Stack <Integer> st = new Stack<>();
+        int n = arr.length;
+        int [] res = new int[n];
+        res[0] = -1;
+        st.push(arr[0]);
+        for(int i = 1; i<n; i++){
+            while(!st.isEmpty() && st.peek()<=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()) res[i] = -1;
+            else res[i] = st.peek();
+            st.push(arr[i]);
+        }
+        for(int i = 0; i<n; i++){
+            System.out.print(res[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        pge(arr);
+    }
+}
